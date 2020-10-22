28a0966d69b2c5f7282554886897fac85e5a43e6
Ananda <anhon@elektro.dtu.dk>
2020-10-22 11:03:38 +0200

    Removed unused stuff from lt tracker and a new tracker using CSRT tracker

--- scripts/LT_tracker.py
+++ scripts/LT_tracker.py
@@ -2,33 +2,16 @@
 import numpy as np
 import cv2 as cv2
 import math
-import roslib
 import rospy
 from geometry_msgs.msg import Point, Twist
-# from geometry_msgs.msg import Pose
-from std_msgs.msg  import Bool
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-
-"""
-From the target point given by the user interface, this program computes the next target point
-
-The constant C_MID denotes the center of the image at 1/2 width and 1/4 height
-The file_name is the output of the .csv file containing the total number of mathces avaiable on the frame, the number of used matches for the frame, 
-as well as the estimated distance to the wall 
-"""
-
-#C_MID = (320, 240)
 C_MID = (640, 360)
-#C_MID = (400, 224)
-
-file_name = 'test.csv'
 
 class Target_tracker():
     def __init__(self):
-        #self.loop_rate = rospy.Rate(20)
-
+        
         #for converting to openCV
         self.bridge = CvBridge()
 
@@ -38,9 +21,6 @@ class Target_tracker():
 
         #the different frames
         self.frame = None
-        self.previous_frame = None
-
-        self.no_previous_frame = True
 
         self.no_change = 0
 
@@ -53,29 +33,23 @@ class Target_tracker():
         self.distance_to_wall = None
         self.wall_angle = None
         self.distance_error = Point()
-        # self.pix_distance = 0.0
-        # self.pix_distance_prev = None
 
         self.first_flag = True
 
         # for csv
         self.matches_array = []
         self.matches_used_array = [] 
-        self.distance_array = []
         
         # publisher
         self.target_pub = rospy.Publisher('/target', Point, queue_size=1)
-        # self.distance_pub = rospy.Publisher('/distance', Pose, queue_size=1)
-        # self.target_tracking_enable = rospy.Publisher('/target_tracking_enable', Bool, queue_size= 1)
         self.distance_error_pub = rospy.Publisher('/distance_error', Point, queue_size=1)
 
         # subscriber
-        #self.image_sub = rospy.Subscriber("/camera/image_decompressed",Image,self.read_frame)
+        # self.image_sub = rospy.Subscriber("/camera/image_decompressed",Image,self.read_frame)
         self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.read_frame)
         self.gui_target_sub = rospy.Subscriber("/gui_target", Point, self.read_gui_target)
         self.distance_sub = rospy.Subscriber("/dtu_controller/current_frame_pose", Twist, self.update_distance)
         
-
         print('Target tracking initialised')
 
     def read_frame(self, data):
@@ -83,10 +57,6 @@ class Target_tracker():
         try:
             self.frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
             self.first_flag = False
-            # for when it runs for the first time
-            if (self.no_previous_frame):
-                self.previous_frame = self.frame[:]
-                self.no_previous_frame = False
 
         except CvBridgeError as e:
             print(e)
@@ -134,10 +104,8 @@ class Target_tracker():
                 coordinates_best_matches_previous_frame.append((x1, y1))
                 coordinates_best_matches_frame.append((x2, y2))
         self.matches_array.append(len(matches))
-        self.matches_used_array.append(len(coordinates_best_matches_frame)) 
-        # self.distance_array.append(self.distance_to_wall)
+        self.matches_used_array.append(len(coordinates_best_matches_frame))
 
-        # np.savetxt(file_name, np.transpose([np.array(self.matches_array), np.array(self.matches_used_array), np.array(self.distance_array)]), delimiter=',',fmt="%s") 
         self.keypoints_previous_frame = keypoints_frame
         self.descriptors_previous_frame = descriptors_frame
 
@@ -192,12 +160,9 @@ class Target_tracker():
             p.x = float(self.new_target[0])
             p.y = float(self.new_target[1])
             p.z = self.no_change
-            #p.orientation.x = 0.0
-            #p.orientation.x = 0.0
-            #p.orientation.x = 0.0
-            #p.orientation.w = 1.0
+
             self.target_pub.publish(p)
-        if self.distance_to_wall != None:
+        if self.distance_to_wall != None and self.no_change:
             self.distance_error_pub.publish(self.distance_error)
 
     def update_distance(self, data):
@@ -208,20 +173,10 @@ class Target_tracker():
         while not rospy.is_shutdown():
 
             if self.new_target[0] != None:
-                # self.target_tracking_enable.publish(True)
                 if(not self.first_flag):
                     self.find_new_target()
                     self.calculate_error()
                     self.publish_new_target()
-
-                    self.previous_frame = self.frame[:]
-                    #print(self.frame.shape)
-                    
-
-                # self.loop_rate.sleep()
-            # else:
-                # self.target_tracking_enable.publish(False)
-                # rospy.Rate(30).sleep()
 
             rospy.Rate(30).sleep()
 
